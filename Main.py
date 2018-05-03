8e7e6666372cc7b338e0d958f347914e7fa481df
Drake <drake11weaver@gmail.com>
2018-05-03 13:10:41 -0500

    servo_straightline

--- Main.py
+++ Main.py
@@ -20,6 +20,18 @@ class Robot():
 		# start timer in case of back_track
 		# if both photoresistors read a wall stop timer and call back_track()
 		# if finished call set_solution
+		GPIO.setwarnings(False)
+		GPIO.setmode (GPIO.BCM)
+		GPIO.setup(18,GPIO.OUT)
+		GPIO.setup(19,GPIO.OUT)
+		q = GPIO.PWM(19,50)
+		p = GPIO.PWM(18,50)
+		p.start(10)
+		q.start(10)
+		time.sleep(5)
+		p.stop()
+		q.stop()
+		GPIO.cleanup()
 		pass
 		
 	def backtrack():
@@ -34,6 +46,14 @@ class Robot():
 class Maze():
 	def __init__(self):
 		self.solution = []
+		
+class tree():
+	def __init__(self, decision):
+		self.data = decision
+		self.left = None
+		self.right = None
+		
+		
 		
 	
 		
